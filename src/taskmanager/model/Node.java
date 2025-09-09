247aa5a1b05f365a78fa7dee377b44036c49532f
AlexD1533 <zeed1533@yandex.ru>
2025-09-09 11:04:58 +0300

    fix gitHub errors codestyle3

--- src/taskmanager/model/Node.java
+++ src/taskmanager/model/Node.java
@@ -1,11 +1,11 @@
 package taskmanager.model;
 
-public class Node<Task> {
-    public Task data;
-    public Node<Task> next;
-    public Node<Task> prev;
+public class Node<T> {
+    public T data;
+    public Node<T> next;
+    public Node<T> prev;
 
-    public Node(Task data) {
+    public Node(T data) {
         this.data = data;
     }
 
