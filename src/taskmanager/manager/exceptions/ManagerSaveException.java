f690ccb98b7a5bab66ee8719e5f5200bc90c10cc
AlexD1533 <zeed1533@yandex.ru>
2025-09-23 16:56:19 +0300

    fix codeStyle2

--- src/taskmanager/manager/exceptions/ManagerSaveException.java
+++ src/taskmanager/manager/exceptions/ManagerSaveException.java
@@ -1,8 +1,7 @@
 package taskmanager.manager.exceptions;
 
-import java.io.IOException;
-
 public class ManagerSaveException extends RuntimeException {
+
     public ManagerSaveException (String message) {
         super(message);
     }
